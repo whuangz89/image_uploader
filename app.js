200788e498c462dd53faba1826feb1e050f58023
William <n.hua.drt@gmail.com>
2019-05-18 17:12:00 +0700

    Add image URL processing

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require("express");
-const sharp = require("sharp");
+const Jimp = require("jimp")
 
 const app = express();
 
+//GET Image URL
+app.get("/:image", (req, res, next) => {
+	var image = req.params.image;
+	var width = parseInt(req.query.width);
+	var height = parseInt(req.query.height);
 
-app.get("/", (req, res, next) => {
-	console.log("Starting...")
+	if (isNaN(width) || width == 0){
+		width = Jimp.AUTO
+	}
+
+	if (isNaN(height) || height == 0){
+		height = Jimp.AUTO
+	}
+
+	if (width == -1 && height == -1){
+		res.send('<img src="'+image+'">')
+	}else{
+		Jimp.read(image, (err, img) => {
+			if (err) throw err;
+			img.resize(width,height).getBase64(Jimp.AUTO, (e, img64)=> {
+				if (e) throw e;
+				res.send('<img src="'+img64+'">')
+			});
+		});	
+	}
+	
+});
+
+app.post("/upload", (req, res, next) => {
+
 });
 
 
